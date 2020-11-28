485ea22a5d35cf21c6d96d1a71eac0cfcd2416c1
6joeb <joe6ball@gmail.com>
2020-11-28 22:54:20 +0000

    user registration

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-const connectDB = async () =>{
-    try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+		});
 
-        console.log('MongoDB connected...');
-    } catch(err) {
-        console.error(err.message);
-        // exit process with failure
-        process.exit(1);
-    }
-}
+		console.log("MongoDB connected...");
+	} catch (err) {
+		console.error(err.message);
+		// exit process with failure
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 // @route   POST api/users
@@ -27,8 +29,34 @@ router.post(
 		const { name, email, password } = req.body;
 
 		try {
-			let user;
-			res.send("User route");
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
+
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+			});
+
+			user = new User({
+				name,
+				email,
+				password,
+				avatar,
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			res.send("User registered");
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");
