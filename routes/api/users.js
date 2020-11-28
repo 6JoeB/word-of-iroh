4203ac4a7213147547abca4789c94f8cc8a977d6
6joeb <joe6ball@gmail.com>
2020-11-28 22:05:55 +0000

    changed indentation to tabs

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,29 +2,38 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 
+const User = require("../../models/User");
 // @route   POST api/users
 // @desc    register user
 // @access  Public
 router.post(
-  "/",
-  [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please enter valid email").isEmail(),
-    check(
-      "password",
-      "Please enter valid password with 6 or more characters"
-    ).isLength({
-      min: 6,
-    }),
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    console.log(req.body);
-    res.send("User route");
-  }
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please enter valid email").isEmail(),
+		check(
+			"password",
+			"Please enter valid password with 6 or more characters"
+		).isLength({
+			min: 6,
+		}),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user;
+			res.send("User route");
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
 );
 
 module.exports = router;
