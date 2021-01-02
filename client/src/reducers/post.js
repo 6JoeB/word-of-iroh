3d9fb8c47e394b17b2dd498192c09f4dcd3fed81
6joeb <joe6ball@gmail.com>
2021-01-02 21:12:02 +0000

    created post item component

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -14,7 +14,7 @@ export default function (state = initialState, action) {
 		case GET_POSTS:
 			return {
 				...state,
-				post: payload,
+				posts: payload,
 				loading: false,
 			};
 		case POST_ERROR:
