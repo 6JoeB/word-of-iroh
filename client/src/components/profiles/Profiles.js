43b88cec4b53e2b7ebd54322e1571dfd487cc470
6joeb <joe6ball@gmail.com>
2021-01-08 23:38:01 +0000

    updated users ui

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -15,10 +15,11 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
 				<Spinner />
 			) : (
 				<Fragment>
-					<h1 className='large text-primary'>Users</h1>
-					<p className='lead'>
-						<i className='fas fa-user-friends'></i> Discover and connect with other users
-					</p>
+					<h1 className='text-primary'>
+						{" "}
+						<i className='fas fa-user-friends'></i> Discover other users
+					</h1>
+					<div className='line'></div>
 					<div className='profiles'>
 						{profiles.length > 0 ? (
 							profiles.map((profile) => (
