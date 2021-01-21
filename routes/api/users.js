98917da4e614137608c922a7d53874faa221275b
6joeb <joe6ball@gmail.com>
2021-01-21 11:59:25 +0000

    created send password reset email backend function

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
+const nodemailer = require("nodemailer");
+const resetPasswordEmailPassword = config.get("resetPasswordEmailPassword");
+const resetPasswordEmail = config.get("resetPasswordEmail");
 
 const User = require("../../models/User");
 // @route   POST api/users
@@ -15,10 +18,7 @@ router.post(
 	[
 		check("name", "Name is required").not().isEmpty(),
 		check("email", "Please enter valid email").isEmail(),
-		check(
-			"password",
-			"Please enter valid password with 6 or more characters"
-		).isLength({
+		check("password", "Please enter valid password with 6 or more characters").isLength({
 			min: 6,
 		}),
 	],
@@ -34,9 +34,7 @@ router.post(
 			let user = await User.findOne({ email });
 
 			if (user) {
-				return res
-					.status(400)
-					.json({ errors: [{ msg: "User already exists" }] });
+				return res.status(400).json({ errors: [{ msg: "User already exists" }] });
 			}
 
 			const avatar = gravatar.url(email, {
@@ -81,5 +79,49 @@ router.post(
 		}
 	}
 );
+
+// @route   get api/users/password-reset-email
+// @desc    check if a user exists and send them a reset password email
+// @access  Public
+router.get("/password-reset-email", async (req, res) => {
+	const email = req.body.email.toLowerCase();
+
+	try {
+		let user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(400).json({ errors: [{ msg: "Invalid email" }] });
+		}
+
+		// Send password reset email
+		var transport = nodemailer.createTransport({
+			service: "Gmail",
+			auth: {
+				user: resetPasswordEmail,
+				pass: resetPasswordEmailPassword,
+			},
+		});
+
+		var mailOptions = {
+			from: '"Team at Word Of Iroh" <WordOfIroh@gmail.com>',
+			to: "",
+			subject: "Nice Nodemailer test",
+			text: "Hey there, it’s our first message sent with Nodemailer",
+			html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer",
+		};
+
+		transport.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log("Message sent: %s", info.messageId);
+		});
+
+		return res.status(200).send("Message sent");
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server error");
+	}
+});
 
 module.exports = router;
